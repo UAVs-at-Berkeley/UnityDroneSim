4f712030373961ebbd3e058aaec8d42e537d8cd8
Alex Chan <alex.chan@berkeley.edu>
2018-02-22 17:50:17 -0800

    Camera follow script and fixed velocity physics

---
+++ Assets/CameraFollowScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour {
+
+    private Transform drone;
+    void Awake(){
+        drone = GameObject.FindGameObjectWithTag("Player").transform; 
+    }
+
+    private Vector3 velocityCameraFollow;
+    public Vector3 behindPosition = new Vector3(0,2,-4);
+    public float angle;
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, drone.transform.TransformPoint(behindPosition) + Vector3.up * Input.GetAxis("Vertical"), ref velocityCameraFollow, .1f);
+        transform.rotation = Quaternion.Euler(new Vector3(angle, drone.GetComponent<DroneMovementScript>().currentYRotation, 0));
+    }
+}
