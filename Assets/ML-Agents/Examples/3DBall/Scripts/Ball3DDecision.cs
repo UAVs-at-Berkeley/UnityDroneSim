ce53b6c0f1a3311402d81e038e00ffedecd999f6
suneelbelkhale <neelus.genius@gmail.com>
2018-04-19 21:13:10 -0700

    upgraded to api3

--- Assets/ML-Agents/Examples/3DBall/Scripts/Ball3DDecision.cs
+++ Assets/ML-Agents/Examples/3DBall/Scripts/Ball3DDecision.cs
@@ -1,24 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball3DDecision : MonoBehaviour, Decision
 {
-    public float[] Decide(List<float> state, List<Camera> observation, float reward, bool done, float[] memory)
-    {
-        if (gameObject.GetComponent<Brain>().brainParameters.actionSpaceType == StateType.continuous)
-        {
-            return new float[4]{ 0f, 0f, 0f, 0.0f };
+    public float rotationSpeed = 2f;
 
-        }
-        else
+    public float[] Decide(
+        List<float> vectorObs,
+        List<Texture2D> visualObs,
+        float reward,
+        bool done,
+        List<float> memory)
+    {
+        if (gameObject.GetComponent<Brain>().brainParameters.vectorActionSpaceType
+            == SpaceType.continuous)
         {
-            return new float[1]{ 1f };
+            List<float> act = new List<float>();
+
+            // state[5] is the velocity of the ball in the x orientation. 
+            // We use this number to control the Platform's z axis rotation speed, 
+            // so that the Platform is tilted in the x orientation correspondingly. 
+            act.Add(vectorObs[5] * rotationSpeed);
+
+            // state[7] is the velocity of the ball in the z orientation. 
+            // We use this number to control the Platform's x axis rotation speed,  
+            // so that the Platform is tilted in the z orientation correspondingly. 
+            act.Add(-vectorObs[7] * rotationSpeed);
+
+            return act.ToArray();
         }
+
+        // If the vector action space type is discrete, then we don't do anything.     
+        return new float[1] { 1f };
     }
 
-    public float[] MakeMemory(List<float> state, List<Camera> observation, float reward, bool done, float[] memory)
+    public List<float> MakeMemory(
+        List<float> vectorObs,
+        List<Texture2D> visualObs,
+        float reward,
+        bool done,
+        List<float> memory)
     {
-        return new float[0];
+        return new List<float>();
     }
 }
